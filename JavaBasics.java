c3a97fddc9642d95b44aeeb0112fb4b5b1c3ece5
Tom <103785835+Ubaidullahqureshi@users.noreply.github.com>
2023-01-25 13:48:47 +0500

    Update JavaBasics.java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,7 +1,7 @@
 
 Java       First   Program     !  
  
-public class  JavaBasics        {
+public  class  JavaBasics        {
     public static void  main    ( String args [ ] ) {
         System.out.println ( " Hello     World " );
     }
